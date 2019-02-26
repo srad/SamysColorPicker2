3bad4561f689ee906fb780a1c2be96b0720aa916
srad <ssedighi@posteo.de>
2019-02-27 00:31:33 +0100

    * Change: Renaming main form. * Fix: Preventing .Dispose on Null.

---
+++ SamysColorPicker2/MainForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace SamysColorPicker2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Screenshot = new System.Windows.Forms.PictureBox();
+            this.SamysNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.ColorValue = new System.Windows.Forms.ToolStripTextBox();
+            this.CopyToolbar = new System.Windows.Forms.ToolStripButton();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.StatusBar = new System.Windows.Forms.StatusStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.Screenshot)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.toolStripContainer1.BottomToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Screenshot
+            // 
+            this.Screenshot.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Screenshot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Screenshot.Location = new System.Drawing.Point(0, 0);
+            this.Screenshot.Name = "Screenshot";
+            this.Screenshot.Size = new System.Drawing.Size(800, 403);
+            this.Screenshot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Screenshot.TabIndex = 0;
+            this.Screenshot.TabStop = false;
+            this.Screenshot.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Pick);
+            // 
+            // SamysNotifyIcon
+            // 
+            this.SamysNotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("SamysNotifyIcon.Icon")));
+            this.SamysNotifyIcon.Text = "notifyIcon1";
+            this.SamysNotifyIcon.Visible = true;
+            this.SamysNotifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.SamysNotifyIcon_MouseClick);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ColorValue,
+            this.CopyToolbar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.Stretch = true;
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // ColorValue
+            // 
+            this.ColorValue.Name = "ColorValue";
+            this.ColorValue.ReadOnly = true;
+            this.ColorValue.Size = new System.Drawing.Size(100, 25);
+            // 
+            // CopyToolbar
+            // 
+            this.CopyToolbar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.CopyToolbar.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CopyToolbar.Image = ((System.Drawing.Image)(resources.GetObject("CopyToolbar.Image")));
+            this.CopyToolbar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.CopyToolbar.Name = "CopyToolbar";
+            this.CopyToolbar.Size = new System.Drawing.Size(42, 22);
+            this.CopyToolbar.Text = "Copy";
+            this.CopyToolbar.Click += new System.EventHandler(this.CopyTextBtn_Click);
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.BottomToolStripPanel
+            // 
+            this.toolStripContainer1.BottomToolStripPanel.Controls.Add(this.StatusBar);
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.Screenshot);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(800, 403);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(800, 450);
+            this.toolStripContainer1.TabIndex = 4;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // StatusBar
+            // 
+            this.StatusBar.Dock = System.Windows.Forms.DockStyle.None;
+            this.StatusBar.Location = new System.Drawing.Point(0, 0);
+            this.StatusBar.Name = "StatusBar";
+            this.StatusBar.Size = new System.Drawing.Size(800, 22);
+            this.StatusBar.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Sam\'s Color Picker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.Screenshot)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.toolStripContainer1.BottomToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.BottomToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Screenshot;
+        private System.Windows.Forms.NotifyIcon SamysNotifyIcon;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripTextBox ColorValue;
+        private System.Windows.Forms.ToolStripButton CopyToolbar;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.StatusStrip StatusBar;
+    }
+}
+
---
+++ SamysColorPicker2/MainForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SamysColorPicker2
+{
+    public partial class MainForm : Form
+    {
+        private Graphics ScreenCapture;
+        private Bitmap ScreenBitmap;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SamysNotifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            CaptureScreen();
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            Screenshot.Image = ScreenBitmap;
+        }
+
+        private void CaptureScreen()
+        {
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            ScreenBitmap = new Bitmap(bounds.Width, bounds.Height);
+            ScreenCapture = Graphics.FromImage(ScreenBitmap);
+            ScreenCapture.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (ScreenCapture != null) { ScreenCapture.Dispose(); }
+            if (ScreenBitmap != null) { ScreenBitmap.Dispose(); }
+        }
+
+        private void Pick(object sender, MouseEventArgs e)
+        {
+            if (ScreenBitmap != null && e.Button == MouseButtons.Left)
+            {
+                var color = GetColorAt(e.Location);
+                ColorValue.Text = HexConverter(color);
+                StatusBar.BackColor = color;
+            }
+        }
+
+        private Color GetColorAt(Point point)
+        {
+            float stretch_X = ScreenBitmap.Width / (float)Screenshot.Width;
+            float stretch_Y = ScreenBitmap.Height / (float)Screenshot.Height;
+            var color = ScreenBitmap.GetPixel((int)(point.X * stretch_X), (int)(point.Y * stretch_Y));
+
+            return color;
+        }
+
+        private static String HexConverter(System.Drawing.Color c)
+        {
+            return "#" + c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
+        }
+
+        private void Copy(object sender, MouseEventArgs e)
+        {
+            Clipboard.SetText(ColorValue.Text);
+        }
+
+        private void CopyTextBtn_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(ColorValue.Text))
+            {
+                Clipboard.SetText(ColorValue.Text);
+            }
+        }
+    }
+}
